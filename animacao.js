97c5d8a4dd2cff05cf31af47e550d7a4785b2fd9
Daniel Martins <53620733+Ezuros@users.noreply.github.com>
2022-05-25 18:11:39 -0300

    Button update

--- animacao.js
+++ animacao.js
@@ -2,9 +2,19 @@ const divLampada = document.querySelector('div.lampada')
 const btnAddRemover = document.querySelector('#adicionar-remover')
 const divIluminacao = document.querySelector('.iluminacao')
 
+const btnImg = document.querySelector('#btn-img')
+
 
 btnAddRemover.addEventListener('click', () =>{
     divLampada.classList.toggle('ligado')
-    divIluminacao.classList.toggle('clarear') 
+    divIluminacao.classList.toggle('clarear')
+
+    if(divLampada.classList.contains('ligado')){
+        btnImg.setAttribute('src', 'img/interruptor2.png')
+    }
+    else{
+        btnImg.setAttribute('src', 'img/interruptor1.png')
+    }
+    
 })
 
